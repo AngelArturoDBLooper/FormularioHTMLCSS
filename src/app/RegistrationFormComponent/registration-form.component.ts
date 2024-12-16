ee3c906796ba72c03f628a529aa42e02d12b8cb3
AngelArturoDBLooper <mendezaarturo@hotmail.com>
2024-12-16 06:35:38 -0500

    Reorganización de los componentes

--- src/app/RegistrationFormComponent/registration-form.component.ts
+++ src/app/RegistrationFormComponent/registration-form.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { AccountTypeComponent } from './account-type/account-type.component';
+import { AdditionalInfoComponent } from './additional-info/additional-info.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
 
 @Component({
   selector: 'registration-form',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, PersonalInfoComponent, AccountTypeComponent, AdditionalInfoComponent, TermsAndConditionsComponent, SubmitButtonComponent],
   templateUrl: './registration-form.component.html',
   styleUrl: './registration-form.component.css'
 })
--- src/app/RegistrationFormComponent/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/RegistrationFormComponent/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-terms-and-conditions',
+  selector: 'terms-and-conditions',
   standalone: true,
   imports: [],
   templateUrl: './terms-and-conditions.component.html',
